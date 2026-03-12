a75ad11e628ca36e3f32c8ea01a9b5b840fa6394
Владимир Воронков <temlord34@gmail.com>
2026-03-12 12:40:34 +0500

    Работа 2.3

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using static System.Math;
-using static System.Console;
-using static System.Convert;
+using System.Diagnostics.Eventing.Reader;
+using System.Runtime.Remoting.Messaging;
 using System.Security.Policy;
+using static System.Console;
 
 
 namespace ConsoleAppEdu
@@ -11,51 +11,85 @@ namespace ConsoleAppEdu
     {
         static void Main(string[] args)
         {
-            WriteLine("Введите числа a, b / x, y");
-            double x = SaveConvert(ReadLine());
-            double y = SaveConvert(ReadLine());
 
-            // Работа аудиторная
-            WriteLine($"{x} + {y} = {x + y}");
-            WriteLine($"{x} - {y} = {x - y}");
-            WriteLine($"{y} - {x} = {y - x}");
-            WriteLine($"{x} * {y} = {x * y}");
-            WriteLine($"{x} / {y} = {x / y}");
-            WriteLine($"log2 = {Round(Log(x, 2),3)}");
-            WriteLine($"ln = {Round(Log(x), 3)}");
-            WriteLine($"log10 = {Round(Log10(x), 3)}");
-            WriteLine($"Экспонента: {E}");
-            WriteLine($"Число {x} в двоичной системе: {Convert.ToString(ToInt16(Abs(x)), 2)}");
-            WriteLine($"Число {y} в двоичной системе: {Convert.ToString(ToInt16(Abs(y)), 2)}");
+            WriteLine("Введите три числа x, y, z: ");
+            double x = RepeatRead();
+            double y = RepeatRead();
+            double z = RepeatRead();
+            double mean = (x + y + z) / 3;
+            double squareSum = (x * z) + (y * y) + (z * z);
 
-            // Работа 2.2
-            double numeratorP1 = (Pow(x * y, 2)) * (y - x);
-            double numeratorP2 = (Pow(Sin(Pow(y + 2 * x, 2)), 3));
-            double denominatorP1 = (Abs(Exp(Abs(Cos(x) + y))));
-            double denominatorP2 = Pow(Cos(Pow((x + 5 * y), 3)), 2);
-            double denominatorP3 = Pow(Tan(Pow(5 * x, 2)), 3);
-            double result = Round(((numeratorP1 * numeratorP2) / ((denominatorP1 * denominatorP2) - denominatorP3)), 3);
-            WriteLine(result);
 
-        }
-        // Статический метод для безопасной конвертации без вылета программы.
-        // Возвращает 0, если не удалось перевести значение или пользовательский ввод оказался неудачным.
-        static double SaveConvert(string str)
-        {
-            if (double.TryParse(PerToCom(str), out double result))
+            if (x > y && x > z) // max x
             {
-                return double.Parse(PerToCom(str));
+                if (y < z) // min y
+                {
+                    WriteLine($"!{mean}!, !{squareSum}!, {z}");
+                }
+                else // min z
+                {
+                    WriteLine($"!{mean}!, {y}, !{squareSum}!");
+                }
+            }
+            if (y > x && y > z) // max y
+            {
+                if (x < z) // min x
+                {
+                    WriteLine($"!{squareSum}!, !{mean}!, {z}"); ;
+                }
+                else // min z
+                {
+                    WriteLine($"{x}, !{mean}!, !{squareSum}!");
+                }
+            }
+            if (z > x && z > y) // max z
+            {
+                if (x < y) // min x
+                {
+                    WriteLine($"!{squareSum}!, {y}, !{mean}!");
+                }
+                else // min y
+                {
+                    WriteLine($"{x}, !{squareSum}!, !{mean}!");
+                }
+            }
+            Write("\n---\tФлажками \"!\" обозначены замененные числа!\t---\n");
+            ReadKey();
+        }
+
+
+        // Статический метод для безопасной конвертации (string -> double) без вылета программы.
+        // Возвращает true, если удалось перевести число, иначе false.
+        // В out-параметр возвращает саму строку после преобразования или double.NaN.
+        static bool SaveConvert(string str, out double result)
+        {
+            if (double.TryParse(str.Replace(".", ","), out double none))
+            {
+                result = double.Parse(str.Replace(".", ","));
+                return true;
             }
             else
             {
-                return 0.0;
+                result = double.NaN;
+                return false;
             }
         }
-        // Статичный метод для перевода точки в запятую при ошибки пользователя при вводе чисел.
-        // Возвращет измененную/исходную строку.
-        static string PerToCom(string str)
+
+
+        // Статический метод для постоянного чтения пользовательского ввода до тех пор, пока не будет введено валидное число.
+        static double RepeatRead()
         {
-            return str.Replace(".", ","); 
+            while (true)
+            {
+                if (SaveConvert(ReadLine(), out double result))
+                {
+                    return result;
+                }
+                else
+                {
+                    WriteLine("Введенное значение не является числом! Повторите попытку.");
+                }
+            }
         }
     }
 }
