454ae7d97d00d8e49b122d43e7eebcec2f5add9c
Владимир Воронков <temlord34@gmail.com>
2026-03-17 09:15:47 +0500

    Задание 2.5

--- Program.cs
+++ Program.cs
@@ -12,14 +12,57 @@ namespace ConsoleAppEdu
     {
         static void Main(string[] args)
         {
+            WriteLine("Введите три числа, коэффициенты уравнения (ax^4 + bx^2 + c): ");
+            double a = RepeatRead();
+            double b = RepeatRead();
+            double c = RepeatRead();
+            double t1, t2, x1, x2;
 
-            WriteLine("Введите аргумент x: ");
-            double x = RepeatRead();
-            double numenator = Abs(Sin(Sqrt(10.5 * x)));
-            double denominator = Pow(x, 2.0 / 3.0) - 0.143;
-            double result = numenator / denominator;
-            WriteLine(result);
-            ReadKey();
+            if (a == 0)
+            {
+                x1 = Sqrt(-c / b);
+                if (x1 != 0)
+                {
+                    WriteLine($"Корни уравнения: ±{x1}");
+                }
+                else
+                {
+                    WriteLine("Корень уравнения: 0");
+                }
+            }
+
+            else
+            {
+                double D = (b * b) - 4 * a * c;
+                if (D >= 0)
+                {
+                    t1 = (-b + Sqrt(D)) / 2 * a;
+                    t2 = (-b - Sqrt(D)) / 2 * a;
+                    x1 = Sqrt(t1);
+                    x2 = Sqrt(t2);
+                    if (t1 > 0 && t2 > 0)
+                    {
+                        WriteLine($"Корни уравнения: ±{x1}, ±{x2}");
+                    }
+                    else if (t1 == 0)
+                    {
+                        WriteLine($"Корни уравнения: 0, ±{x2}");
+                    }
+                    else if (t2 == 0)
+                    {
+                        WriteLine($"Корни уравнения: ±{x1}, 0");
+                    }
+                    else if (t1 < 0 || t2 < 0)
+                    {
+                        WriteLine("Корней нет!");
+                    }
+                }
+
+                else
+                {
+                    WriteLine("Корней нет!");
+                }
+            }
         }
 
 
