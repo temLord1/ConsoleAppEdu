d2d5058ece353b4a4742fdca51fdc2d514f38843
Владимир Воронков <temlord34@gmail.com>
2026-03-19 13:41:30 +0500

    Задание 2.6

--- Program.cs
+++ Program.cs
@@ -6,132 +6,47 @@ using System.Security.Policy;
 
 namespace ConsoleAppEdu
 {
-
-    public class Triangle
-    {
-        public double a { get; private set; }
-        public double b { get; private set; }
-        public double c { get; private set; }
-
-        public Triangle(double side_a, double side_b, double side_c)
-        {
-            a = side_a;
-            b = side_b;
-            c = side_c;
-        }
-
-        public bool IsValid()
-        {
-            return (a + b > c) && (c + a > b) && (c + b > a);
-        }
-
-        public bool IsEquilateral()
-        {
-            return (a == b && b == c);
-        }
-
-        public bool IsIsosceles()
-        {
-            return !IsEquilateral() && (a == b || a == c || b == c);
-        }
-
-        public bool IsRightTriangle()
-        {
-            double pif_a = Math.Sqrt(b * b + c * c);
-            double pif_b = Math.Sqrt(a * a + c * c);
-            double pif_c = Math.Sqrt(a * a + b * b);
-            double epsilon = 0.001;
-
-            return Math.Abs(a - pif_a) < epsilon || Math.Abs(b - pif_b) < epsilon || Math.Abs(c - pif_c) < epsilon;
-        }
-
-        public string GetInfo()
-        {
-            if (!IsValid()) return "Треугольник с такими сторонами не существует!";
-
-            else
-            {
-                string info = "Данный треугольник существует и является ";
-
-                if (IsEquilateral())
-                {
-                    info += "равносторонним";
-                }
-                else if (IsIsosceles())
-                {
-                    info += "равнобедренным";
-                }
-                else
-                {
-                    info += "разносторонним";
-                }
-
-                if (IsRightTriangle())
-                {
-                    info += ", прямоугольным";
-                }
-
-                return info + ".";
-            }
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            double a = GetNumber("Введите длину первой стороны: ");
-            double b = GetNumber("Введите длину второй стороны: ");
-            double c = GetNumber("Введите длину третьей стороны: ");
-
-            Triangle triangle = new Triangle(a, b, c);
-
-            Console.WriteLine("\nРезультат проверки:");
-            Console.WriteLine(triangle.GetInfo());
-
-            if (triangle.IsValid())
-            {
-                Console.WriteLine("\nДополнительная информация:");
-                Console.WriteLine($"Стороны: a = {a}, b = {b}, c = {c}");
-            }
-
-            Console.WriteLine("\nНажмите любую клавишу для выхода...");
-            Console.ReadKey();
-        }
-
-
-        static double GetNumber(string steer)
-        {
-            double number = 0;
+            string russian = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            string vowels = "аеёиоуыэюя";
+            string input = "";
             bool isValid = false;
 
             while (!isValid)
             {
-                Console.Write(steer);
-                string input = Console.ReadLine();
-
+                Console.Write("Введите символ: ");
                 try
                 {
-                    number = double.Parse(input);
-                    if (number <= 0)
+                    char symbol = Char.ToLower(Convert.ToChar(Console.ReadLine()));
+                if (russian.Contains(Convert.ToString(symbol)))
+                {
+                    input += "Введенный символ - ";
+                    if (vowels.Contains(Convert.ToString(symbol)))
                     {
-                        Console.WriteLine("Неверный ввод! Стороны треугольника должны быть положительными!");
+                        input += "гласная ";
                     }
                     else
                     {
-                        isValid = true;
+                        input += "согласная ";
                     }
+                    input += "буква русского языка!";
+                    isValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Этот символ не является русской буквой! ");
+                }
                 }
                 catch (FormatException)
                 {
-                    Console.WriteLine("Ошибка: Введите корректное число!");
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine("Ошибка: Число слишком большое или слишком маленькое, введите корректное число!");
+                    Console.WriteLine("Некорректный ввод!");
                 }
             }
-
-            return number;
+            Console.Write($"{input}\n\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
         }
     }
 }
