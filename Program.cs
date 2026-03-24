5681c46beae1d3714c5b56fc424e5ed9b28cd4a0
Владимир Воронков <temlord34@gmail.com>
2026-03-24 09:12:42 +0500

    Контрольная работа 2.2

--- Program.cs
+++ Program.cs
@@ -2,131 +2,63 @@
 
 namespace ConsoleAppEdu
 {
-
-    public class Triangle
-    {
-        public double a { get; private set; }
-        public double b { get; private set; }
-        public double c { get; private set; }
-
-        public Triangle(double side_a, double side_b, double side_c)
-        {
-            a = side_a;
-            b = side_b;
-            c = side_c;
-        }
-
-        public bool IsValid()
-        {
-            return (a + b > c) && (c + a > b) && (c + b > a);
-        }
-
-        public bool IsEquilateral()
-        {
-            return (a == b && b == c);
-        }
-
-        public bool IsIsosceles()
-        {
-            return !IsEquilateral() && (a == b || a == c || b == c);
-        }
-
-        public bool IsRightTriangle()
-        {
-            double pif_a = Math.Sqrt(b * b + c * c);
-            double pif_b = Math.Sqrt(a * a + c * c);
-            double pif_c = Math.Sqrt(a * a + b * b);
-            double epsilon = 0.001;
-
-            return Math.Abs(a - pif_a) < epsilon || Math.Abs(b - pif_b) < epsilon || Math.Abs(c - pif_c) < epsilon;
-        }
-
-        public string GetInfo()
-        {
-            if (!IsValid()) return "Треугольник с такими сторонами не существует!";
-
-            else
-            {
-                string info = "Данный треугольник существует и является ";
-
-                if (IsEquilateral())
-                {
-                    info += "равносторонним";
-                }
-                else if (IsIsosceles())
-                {
-                    info += "равнобедренным";
-                }
-                else
-                {
-                    info += "разносторонним";
-                }
-
-                if (IsRightTriangle())
-                {
-                    info += ", прямоугольным";
-                }
-
-                return info + ".";
-            }
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            double a = GetNumber("Введите длину первой стороны: ");
-            double b = GetNumber("Введите длину второй стороны: ");
-            double c = GetNumber("Введите длину третьей стороны: ");
-
-            Triangle triangle = new Triangle(a, b, c);
-
-            Console.WriteLine("\nРезультат проверки:");
-            Console.WriteLine(triangle.GetInfo());
-
-            if (triangle.IsValid())
+            while (true)
             {
-                Console.WriteLine("\nДополнительная информация:");
-                Console.WriteLine($"Стороны: a = {a}, b = {b}, c = {c}");
-            }
+                string output = "";
+                double x = GetNumber("Введите координату x точки: ");
+                double y = GetNumber("Введите координату y точки: ");
 
-            Console.WriteLine("\nНажмите любую клавишу для выхода...");
-            Console.ReadKey();
+                // Проверяем, что x^2 + y^2 >= R^2 и подобная же проверка для второй окружности.
+                // Когда сумма квадратов больше радиуса, то точка вне окружности, когда меньше - внутри.
+                if ((Math.Pow(x, 2) + Math.Pow(y, 2) >= 9) && (Math.Pow(x, 2) + Math.Pow(y, 2) <= 144))
+                {
+                    // Чтобы понять, что точка внутри ветвей парраболы, находим значение f(x) и сравниваем его
+                    // с данным y. Пока y >= f(x), точка будет выше ветвей парраболы. А так как a > 0, то внутри ветвей.
+                    // Предположил, что график парраболы - f(x) = x^2 - 20.
+                    double f = Math.Pow(x, 2) - 20;
+                    if (y >= f)
+                    {
+                        output += "Точка принадлежит заштрихованной области!";
+                    }
+                    else
+                    {
+                        output += "Точка не принадлежит выделенной области.";
+                    }
+                }
+                else
+                {
+                    output += "Точка не принадлежит выделенной области.";
+                }
+
+                Console.WriteLine($"{output}\n");
+                Console.ReadKey();
+            }
         }
 
 
-        static double GetNumber(string steer)
+        static double GetNumber(string prompt)
         {
             double number = 0;
             bool isValid = false;
 
             while (!isValid)
             {
-                Console.Write(steer);
+                Console.WriteLine(prompt);
                 string input = Console.ReadLine();
-
-                try
+                bool isNumber = Double.TryParse(input, out number);
+                if (isNumber)
                 {
-                    number = double.Parse(input);
-                    if (number <= 0)
-                    {
-                        Console.WriteLine("Неверный ввод! Стороны треугольника должны быть положительными!");
-                    }
-                    else
-                    {
-                        isValid = true;
-                    }
+                    isValid = true;
                 }
-                catch (FormatException)
+                else
                 {
-                    Console.WriteLine("Ошибка: Введите корректное число!");
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine("Ошибка: Число слишком большое или слишком маленькое, введите корректное число!");
+                    Console.WriteLine("Некорректный ввод!");
                 }
             }
-
             return number;
         }
     }
