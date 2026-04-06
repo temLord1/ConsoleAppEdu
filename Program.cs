044d38b46c41d5181d5ce7115ab1cb13d49db8d6
Владимир Воронков <temlord34@gmail.com>
2026-04-06 14:46:51 +0500

    Задание 2.15

--- Program.cs
+++ Program.cs
@@ -9,91 +9,47 @@ namespace ConsoleAppEdu
         {
             while (true)
             {
-                long n = GetNumber("Введите N: ");
-                string output = "";
-                output += $"1. Количество цифр в числе: {NumberOfDigits(n)}.\n";
-                output += $"2. Сумма цифр в числе: {SumOfDigits(n)}.\n";
-                output += $"3. Оканчивается ли записи числа в dec и hex одной цифрой?: {AreLastDigitsEqual(n)}.\n";
-                output += $"4. {TwoHexDigits(n)}.\n";
-                output += $"5. В шестнадцатиричной записи числа ровно {NumberOfB(n)} символов \"B\".\n";
-                output += $"6. Количество простых делителей числа: {PrimeDivisors(n)}.";
+                long a = GetNumber("Введите левую границу диапазона: "); // [a:b]
+                long b = GetNumber("Введите правую границу диапазона: ");
+                if (a >= b)
+                {
+                    Console.WriteLine("Некорректный диапазон!");
+                    continue;
+                }
 
-                Console.WriteLine(output);
+                for (long i = a; i <= b; i++)
+                {
+                    string output = $"Для числа {i} делители: ";
+
+                    if (i >= -1 && i <= 1)
+                    {
+                        Console.WriteLine($"Для числа {i} Нет делителей");
+                        continue;
+                    }
+                    
+                    for (long j = 2; j <= Math.Abs(i); j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            bool isPrime = true;
+                            for (long k = 2; k < j; k++)
+                            {
+                                if (j % k == 0)
+                                {
+                                    isPrime = false;
+                                    break;
+                                }
+                            }
+                            if (isPrime && j != i) output += $"{j}; ";
+                            if (isPrime && j == Math.Abs(i)) output += $"Число {i} простое";
+                        }
+                    }
+                    Console.WriteLine(output);
+                }
                 Console.ReadKey();
             }
         }
 
-        // 1. Количество цифр в числе
-        static int NumberOfDigits(long number)
-        {
-            int counter = 0;
-            while (number > 0)
-            {
-                number /= 10;
-                counter++;
-            }
-            return counter;
-        }
-
-        // 2. Сумму цифр в числе
-        static int SumOfDigits(long number)
-        {
-            int outputSum = 0;
-            while (number > 0)
-            {
-                outputSum += (int)(number % 10);
-                number /= 10;
-            }
-            return outputSum;
-        }
-
-        // 3. Определить оканчивается ли десятичная и шестнадцатиричная запись этого числа одинаковой цифрой.
-        static bool AreLastDigitsEqual(long number)
-        {
-            if (number % 10 == number % 16) return true;
-            else return false;
-        }
-
-        // 4. Выдать сообщение, если шестнадцатиричная запись числа содержит ровно два символа.
-        static string TwoHexDigits(long number)
-        {
-            if (number >= 16 && number <= 255) return "Шестнадцатиричная запись числа содержит два символа!";
-            else return "Шестнадцатиричная запись числа меньше/больше двух символов";
-        }
-
-        // 5. Определить сколько раз встречается в шестнадцатиричной записи числа символ В
-        static int NumberOfB(long number)
-        {
-            int counter = 0;
-
-            while (number > 0)
-            {
-                if (number % 16 == 11) counter++;
-                number /= 16;
-            }
-            return counter;
-        }
-
-        // 6. Найти количество простых делителей числа N
-        static int PrimeDivisors(long number)
-        {
-            int counter = 0;
-            for (int i = 2;  i <= number; i++)
-            {
-                if (number % i == 0) 
-                {
-                    bool isPrime = true;
-                    for (int j = 2; j < i; j++)
-                    {
-                        if (i % j == 0) isPrime = false;
-                    }
-                    if (isPrime) counter++;
-                }
-            }
-            return counter;
-        }
-
-        // Статический метод для получения числа, что удовлетворяет условию задачи. В этом случае - number > 0.
         static long GetNumber(string prompt)
         {
             long number = 0;
@@ -104,7 +60,7 @@ namespace ConsoleAppEdu
                 Console.Write(prompt);
                 string input = Console.ReadLine();
                 bool isNumber = long.TryParse(input, out number);
-                if (isNumber && number > 0)
+                if (isNumber)
                 {
                     isValid = true;
                 }
