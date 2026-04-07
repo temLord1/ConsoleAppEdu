628d07c6b0b7d7dec04564dbf5814f4c7c0c3eee
Владимир Воронков <temlord34@gmail.com>
2026-04-07 08:50:17 +0500

    Контрольная работа 2.3

--- Program.cs
+++ Program.cs
@@ -9,67 +9,16 @@ namespace ConsoleAppEdu
         {
             while (true)
             {
-                long a = GetNumber("Введите левую границу диапазона: "); // [a:b]
-                long b = GetNumber("Введите правую границу диапазона: ");
-                if (a >= b)
+                double sum = 0;
+                
+                for (double n = 1; n <= 10; n++)
                 {
-                    Console.WriteLine("Некорректный диапазон!");
-                    continue;
+                    sum += (n) / (n + 1);
                 }
-
-                for (long i = a; i <= b; i++)
-                {
-                    string output = $"Для числа {i} делители: ";
-
-                    if (i >= -1 && i <= 1)
-                    {
-                        Console.WriteLine($"Для числа {i} Нет делителей");
-                        continue;
-                    }
-                    
-                    for (long j = 2; j <= Math.Abs(i); j++)
-                    {
-                        if (i % j == 0)
-                        {
-                            bool isPrime = true;
-                            for (long k = 2; k < j; k++)
-                            {
-                                if (j % k == 0)
-                                {
-                                    isPrime = false;
-                                    break;
-                                }
-                            }
-                            if (isPrime && j != i) output += $"{j}; ";
-                            if (isPrime && j == Math.Abs(i)) output += $"Число {i} простое";
-                        }
-                    }
-                    Console.WriteLine(output);
-                }
+                
+                Console.WriteLine($"Вариант 23\nСумма ряда: {sum:F5}");
                 Console.ReadKey();
             }
-        }
-
-        static long GetNumber(string prompt)
-        {
-            long number = 0;
-            bool isValid = false;
-
-            while (!isValid)
-            {
-                Console.Write(prompt);
-                string input = Console.ReadLine();
-                bool isNumber = long.TryParse(input, out number);
-                if (isNumber)
-                {
-                    isValid = true;
-                }
-                else
-                {
-                    Console.WriteLine("Некорректный ввод!");
-                }
-            }
-            return number;
         }
     }
 }
